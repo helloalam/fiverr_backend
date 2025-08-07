ca5e700fc718463836eb6dede2c2670ed265eadb
helloalam <sahjadalam849@gmail.com>
2025-08-08 03:25:47 +0530

    fix cookies

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -19,12 +19,13 @@ export const register = async (req, res, next) => {
 };
 export const login = async (req, res, next) => {
   try {
-    const user = await User.findOne({ email: req.body.email });
+    const user = await User.findOne({ username: req.body.username });
 
     if (!user) return next(createError(404, "User not found!"));
 
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
-    if (!isCorrect) return next(createError(400, "Wrong password or username!"));
+    if (!isCorrect)
+      return next(createError(400, "Wrong password or username!"));
 
     const token = jwt.sign(
       {
@@ -34,20 +35,17 @@ export const login = async (req, res, next) => {
       process.env.JWT_SECRET
     );
 
-    const { password, ...userWithoutPassword } = user._doc;
-
-    // ✅ Set secure access token in cookie
-    res.cookie("accessToken", token, {
-      httpOnly: true,
-      sameSite: "none",
-      secure: true,
-    }).status(200).send(userWithoutPassword);
-
+    const { password, ...info } = user._doc;
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
   } catch (err) {
     next(err);
   }
 };
-
 
 export const logout = async (req, res) => {
   res
